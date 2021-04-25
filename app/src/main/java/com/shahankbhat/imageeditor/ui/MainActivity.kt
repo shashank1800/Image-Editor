31b73ac50d0d2f79dd08c62fd3dd23b6a8c6b071
Shashank Bhat <shashank@orchids.edu.in>
2021-04-25 12:17:35 +0530

    initial commit

--- app/src/main/java/com/shahankbhat/imageeditor/ui/MainActivity.kt
+++ app/src/main/java/com/shahankbhat/imageeditor/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.shahankbhat.imageeditor.ui
 
+import android.annotation.SuppressLint
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Canvas
@@ -7,6 +8,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
@@ -16,9 +18,12 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.GetContent
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
+import androidx.exifinterface.media.ExifInterface
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.shahankbhat.imageeditor.PointXY
 import com.shahankbhat.imageeditor.R
 import com.shahankbhat.imageeditor.databinding.ActivityMainBinding
@@ -26,18 +31,21 @@ import com.shahankbhat.imageeditor.deltaX
 import com.shahankbhat.imageeditor.deltaY
 import com.shahankbhat.imageeditor.util.EditorFunctions.flipBitmap
 import com.shahankbhat.imageeditor.util.EditorFunctions.rotateBitmap
+import com.shahankbhat.imageeditor.util.saveBitmapToStorage
 import com.shahankbhat.imageeditor.viewmodel.MainActivityViewModel
-import java.io.File
+import java.io.*
 import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchListener {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainActivityViewModel by viewModels()
-    lateinit var selectedImageBitmap: Bitmap
-    lateinit var canvasBitmap: Bitmap
+    private lateinit var selectedImageBitmap: Bitmap
+    private lateinit var canvasBitmap: Bitmap
+    private lateinit var exifInterface : ExifInterface
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -48,13 +56,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
                 viewModel.isImageAvailable.set(true)
                 binding.imageView.setImageURI(it)
 
+                getExifInformation(it)
                 selectedImageChanged()
+                drawCropLine()
             }
         })
 
         initOnClickListener()
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun getExifInformation(uri: Uri?){
+        try {
+            uri?.let { url ->
+                val inOutStream = contentResolver.openInputStream(url)
+                inOutStream?.let {
+                    exifInterface = ExifInterface(it)
+                }
+            }
+        } catch (ex : IOException) { }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onClick(v: View?) {
         when (v?.id) {
 
@@ -63,7 +86,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
             }
 
             binding.btnCamera.id -> {
-                val file = File(filesDir, "ImageEditor")
+                val file = File(filesDir, IMAGES_FOLDER_NAME)
                 if (!file.exists())
                     file.mkdir()
 
@@ -117,9 +140,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
             binding.btnCropSubmit.id -> {
 
                 val ratio = getRatioOfWH()
-                if (ratio < 2) {
+                if (ratio < 3) {
                     viewModel.isCropEnabled.set(false)
                     binding.imageView.setOnTouchListener(null)
+
+                    if(left.toInt() < 0)
+                        left = 0f
+                    if(top.toInt() < 0)
+                        top = 0f
+                    if(right > selectedImageBitmap.width)
+                        right = selectedImageBitmap.width.toFloat()
+                    if(bottom > selectedImageBitmap.height)
+                        bottom = selectedImageBitmap.height.toFloat()
 
                     selectedImageBitmap = Bitmap.createBitmap(
                         selectedImageBitmap,
@@ -141,7 +173,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
 
                     binding.imageView.setImageBitmap(selectedImageBitmap)
                 } else {
-                    Toast.makeText(this, "Ratio should not be ", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this,
+                        "Height to Width ratio should be below 3",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
@@ -157,11 +193,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
     }
 
 
-    val getContent = registerForActivityResult(GetContent()) { uri: Uri? ->
+    private val getContent = registerForActivityResult(GetContent()) { uri: Uri? ->
         viewModel.imageUri.value = uri
     }
 
-    val takePicture = registerForActivityResult(
+    private val takePicture = registerForActivityResult(
         ActivityResultContracts.TakePicture()
     ) {
 
@@ -178,21 +214,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
         val smallerSide =
             if (selectedImageBitmap.width < selectedImageBitmap.height) selectedImageBitmap.width else selectedImageBitmap.height
 
-        left = (smallerSide / 2 - smallerSide / 4).toFloat()
-        right = (selectedImageBitmap.width - smallerSide / 4).toFloat()
 
-        top = (smallerSide / 2 - smallerSide / 4).toFloat()
-        bottom = (selectedImageBitmap.height - smallerSide / 4).toFloat()
+        val smallerSideByThree = smallerSide / 3
+        val centerX = selectedImageBitmap.width / 2
+        val centerY = selectedImageBitmap.height / 2
+
+        left = (centerX - smallerSideByThree).toFloat()
+        right = (centerX + smallerSideByThree).toFloat()
+
+        top = (centerY - smallerSideByThree).toFloat()
+        bottom = (centerY + smallerSideByThree).toFloat()
+
+//        left = (smallerSide / 2 - smallerSide / 4).toFloat()
+//        right = (selectedImageBitmap.width - smallerSide / 4).toFloat()
 //
-//        right = if(left > (selectedImageBitmap.width - MIN_WIDTH))
-//            left + MIN_WIDTH
-//        else
-//            selectedImageBitmap.width - MIN_WIDTH
-//
-//        bottom = if(top > (selectedImageBitmap.height - MIN_WIDTH))
-//            top + MIN_WIDTH
-//        else
-//            selectedImageBitmap.height - MIN_WIDTH
+//        top = (smallerSide / 2 - smallerSide / 4).toFloat()
+//        bottom = (selectedImageBitmap.height - smallerSide / 4).toFloat()
     }
 
     private fun drawCropLine() {
@@ -202,18 +239,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
             Bitmap.Config.ARGB_8888
         )
         val canvas = Canvas(canvasBitmap)
-        canvas.drawRect(
-            left,
-            top,
-            right,
-            bottom,
-            linePaint
-        )
+//        canvas.drawRect(
+//            left,
+//            top,
+//            right,
+//            bottom,
+//            linePaint
+//        )
+
+        canvas.drawRect(0f, 0f, left, selectedImageBitmap.height.toFloat(), background)
+        canvas.drawRect(left, 0f, selectedImageBitmap.width.toFloat(), top, background)
+        canvas.drawRect(right, top, selectedImageBitmap.width.toFloat(), selectedImageBitmap.height.toFloat(), background)
+        canvas.drawRect(left, bottom, right, selectedImageBitmap.height.toFloat(), background)
 
         val newBitmap = BitmapDrawable(Resources.getSystem(), canvasBitmap)
         binding.canvas.setImageBitmap(newBitmap.bitmap)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouch(v: View, event: MotionEvent): Boolean {
         val view: ImageView = v as ImageView
 
@@ -233,6 +276,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
          */
         if (startEvent.deltaX(endEvent) > startEvent.deltaY(endEvent)) {
             Log.i("drag", "x axis")
+
+            Log.i("size", "${canvasBitmap.width} - ${selectedImageBitmap.width}")
 
             val deltaLeft = abs(startEvent.x - left)
             val deltaRight = abs(startEvent.x - right)
@@ -288,14 +333,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
     var right = 200f
     var bottom = 200f
 
-    private var linePaint = Paint()
     private var background = Paint()
+//    private var background = Paint()
 
     init {
-        linePaint.color = Color.parseColor("#72050505")
-        background.color = Color.parseColor("#E2303030")
+        background.color = Color.parseColor("#72050505")
+//        background.color = Color.parseColor("#E2303030")
 
-        linePaint.strokeWidth = 20f
+//        linePaint.strokeWidth = 20f
     }
 
     private fun initOnClickListener() {
@@ -318,5 +363,37 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchList
         const val ROTATE_RIGHT = 90f
 
         const val MIN_WIDTH = 200f
+        const val IMAGES_FOLDER_NAME = "ImageEditor"
     }
+
+    override fun onBackPressed() {
+        if (viewModel.isImageAvailable.get()) {
+            val materialAlertDialogBuilder = MaterialAlertDialogBuilder(this)
+            materialAlertDialogBuilder.setTitle("You have unsaved changes")
+                .setPositiveButton(
+                    "Save & Exit"
+                ) { dialog, _ ->
+                    dialog.dismiss()
+
+                    saveBitmapToStorage(selectedImageBitmap, "Image-Editor-${System.currentTimeMillis()}")
+                    viewModel.isImageAvailable.set(false)
+                    viewModel.isCropEnabled.set(false)
+                }
+                .setNegativeButton(
+                    "Discard"
+                ) { dialog, _ ->
+                    dialog.dismiss()
+                    viewModel.isImageAvailable.set(false)
+                    viewModel.isCropEnabled.set(false)
+                }
+
+            materialAlertDialogBuilder.show()
+        } else {
+            finish()
+        }
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/shahankbhat/imageeditor/util/Extensions.kt
@@ -0,0 +1,52 @@
+package com.shahankbhat.imageeditor.util
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.annotation.NonNull
+import com.shahankbhat.imageeditor.ui.MainActivity
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+
+@Throws(IOException::class)
+fun Context.saveBitmapToStorage(bitmap: Bitmap, @NonNull name: String) {
+
+    val saved: Boolean
+    val fos: OutputStream
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val resolver = applicationContext.contentResolver
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+        contentValues.put(
+            MediaStore.MediaColumns.RELATIVE_PATH,
+            "DCIM/${MainActivity.IMAGES_FOLDER_NAME}"
+        )
+        val imageUri =
+            resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        fos = resolver.openOutputStream(imageUri!!)!!
+    } else {
+        val imagesDir = (Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_DCIM
+        ).toString() + File.separator + MainActivity.IMAGES_FOLDER_NAME)
+        val file = File(imagesDir)
+        if (!file.exists()) {
+            file.mkdir()
+        }
+        val image = File(imagesDir, "$name.png")
+        fos = FileOutputStream(image)
+    }
+    saved = bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+    fos.flush()
+    fos.close()
+
+    if (saved)
+        Toast.makeText(this, "Image saved", Toast.LENGTH_SHORT).show()
+
+}
